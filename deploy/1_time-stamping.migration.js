375098b8364f68f4613918ea8f4cc06ce7f0c0db
Ruslan Kasheparov <ruslankasheparov@gmail.com>
2023-02-01 18:14:18 +0100

    Fix deploy script

--- deploy/1_time-stamping.migration.js
+++ deploy/1_time-stamping.migration.js
@@ -1,16 +1,27 @@
+const { poseidonContract } = require("circomlibjs");
 const TimeStamping = artifacts.require("TimeStamping");
 const PublicERC1967Proxy = artifacts.require("PublicERC1967Proxy");
 const HashVerifier = artifacts.require("HashVerifier");
 
 module.exports = async (deployer) => {
+  console.log("Deploying Poseidon Hasher...");
+  const PoseidonHasher = new hre.ethers.ContractFactory(
+    poseidonContract.generateABI(1),
+    poseidonContract.createCode(1),
+    await hre.ethers.getSigner()
+  );
+  const poseidonHasher = await PoseidonHasher.deploy();
+  await poseidonHasher.deployed();
+
   const verifier = await deployer.deploy(HashVerifier);
+
   const timeStampingImpl = await deployer.deploy(TimeStamping);
 
   console.log(`TimeStamping implementation address ----- ${timeStampingImpl.address}`);
   const timeStampingProxy = await deployer.deploy(PublicERC1967Proxy, timeStampingImpl.address, "0x");
   const timeStamping = await TimeStamping.at(timeStampingProxy.address);
 
-  await timeStamping.__TimeStamping_init(verifier.address);
+  await timeStamping.__TimeStamping_init(verifier.address, poseidonHasher.address);
 
   console.log(`TimeStamping address ----- ${timeStamping.address}`);
 };
